833c7d4ef265b20a34140a866e90b916818804b7
darthjee <darthjee@gmail.com>
2019-08-15 21:11:19 +0200

    Add bigger deviances spec

--- spec/lib/dr_light/scientific_number_spec.rb
+++ spec/lib/dr_light/scientific_number_spec.rb
@@ -325,5 +325,115 @@ describe DrLight::ScientificNumber do
         end
       end
     end
+
+    context 'when deviance has bigger order than the value' do
+      context 'when negative number only has 2 digits and order 10' do
+        let(:value)    { -12 }
+        let(:deviance) { 130 }
+
+        it 'shows 2 deviance digits' do
+          expect(number.to_s).to eq('-0.1(13)e2')
+        end
+      end
+
+      xcontext 'when negative number has 3 digits and order 10^2' do
+        let(:value)    { -123 }
+        let(:deviance) { 1.34 }
+
+        it 'shows 2 deviance digits' do
+          expect(number.to_s).to eq('-1.230(13)e2')
+        end
+      end
+
+      xcontext 'when negative number only has 2 digits and order 1' do
+        let(:value)    { -1.2 }
+        let(:deviance) { 0.013 }
+
+        it 'shows 2 deviance digits' do
+          expect(number.to_s).to eq('-1.200(13)')
+        end
+      end
+
+      xcontext 'when negative number has 3 digits and order 1' do
+        let(:value)    { -1.23 }
+        let(:deviance) { 0.0134 }
+
+        it 'shows 2 deviance digits' do
+          expect(number.to_s).to eq('-1.230(13)')
+        end
+      end
+
+      xcontext 'when negative number only has 2 digits and order 10^-1' do
+        let(:value)    { -0.12 }
+        let(:deviance) { 0.0013 }
+
+        it 'shows 2 deviance digits' do
+          expect(number.to_s).to eq('-1.200(13)e-1')
+        end
+      end
+
+      xcontext 'when negative number has 3 digits and order 10^-1' do
+        let(:value)    { -0.123 }
+        let(:deviance) { 0.00134 }
+
+        it 'shows 2 deviance digits' do
+          expect(number.to_s).to eq('-1.230(13)e-1')
+        end
+      end
+
+      xcontext 'when positive number only has 2 digits and order 10' do
+        let(:value)    { 12 }
+        let(:deviance) { 0.13 }
+
+        it 'shows 2 deviance digits' do
+          expect(number.to_s).to eq('1.200(13)e1')
+        end
+      end
+
+      xcontext 'when positive number has 3 digits and order 10^2' do
+        let(:value)    { 123 }
+        let(:deviance) { 1.34 }
+
+        it 'shows 2 deviance digits' do
+          expect(number.to_s).to eq('1.230(13)e2')
+        end
+      end
+
+      xcontext 'when positive number only has 2 digits and order 1' do
+        let(:value)    { 1.2 }
+        let(:deviance) { 0.013 }
+
+        it 'shows 2 deviance digits' do
+          expect(number.to_s).to eq('1.200(13)')
+        end
+      end
+
+      xcontext 'when positive number has 3 digits and order 1' do
+        let(:value)    { 1.23 }
+        let(:deviance) { 0.0134 }
+
+        it 'shows 2 deviance digits' do
+          expect(number.to_s).to eq('1.230(13)')
+        end
+      end
+
+      xcontext 'when positive number only has 2 digits and order 10^-1' do
+        let(:value)    { 0.12 }
+        let(:deviance) { 0.0013 }
+
+        it 'shows 2 deviance digits' do
+          expect(number.to_s).to eq('1.200(13)e-1')
+        end
+      end
+
+      xcontext 'when positive number has 3 digits and order 10^-1' do
+        let(:value) { 0.123 }
+        let(:deviance) { 0.00134 }
+
+        it 'shows 2 deviance digits' do
+          expect(number.to_s).to eq('1.230(13)e-1')
+        end
+      end
+    end
   end
 end
