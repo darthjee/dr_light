41390f8365c3bde30b838bf921b53655baf753a5
darthjee <darthjee@gmail.com>
2019-08-15 16:27:10 +0200

    Add deviance

--- lib/dr_light/scientific_number.rb
+++ lib/dr_light/scientific_number.rb
@@ -13,33 +13,47 @@ module DrLight
       format_value
 
       if exponential.zero?
-        format('%<value>.2g', value: formatted_value)
+        if deviance.zero?
+          format('%<value>.2g', value: formatted_value)
+        else
+          format('%<value>.2g(%<deviance>d)', value: formatted_value, deviance: formatted_deviance * 10)
+        end
       else
-        format(
-          '%<value>.2ge%<exponential>d',
-          value: formatted_value, exponential: exponential
-        )
+        if deviance.zero?
+          format(
+            '%<value>.2ge%<exponential>d',
+            value: formatted_value, exponential: exponential
+          )
+        else
+          format(
+            '%<value>.2g(%<deviance>d)e%<exponential>d',
+            value: formatted_value, exponential: exponential, deviance: formatted_deviance * 10
+          )
+        end
       end
     end
 
     private
 
-    attr_reader :exponential, :formatted_value
+    attr_reader :exponential, :formatted_value, :formatted_deviance
 
     # rubocop:disable Metrics/MethodLength:
     def format_value
       return if @formatted_value
 
       @formatted_value = value.abs
+      @formatted_deviance = deviance.abs
       @exponential = 0
 
       while @formatted_value > 10
         @formatted_value /= 10.0
+        @formatted_deviance /= 10.0
         @exponential += 1
       end
 
       while @formatted_value < 1
-        @formatted_value *= 10.0
+        @formatted_value *= 10
+        @formatted_deviance *= 10
         @exponential -= 1
       end
 
--- spec/lib/dr_light/scientific_number_spec.rb
+++ spec/lib/dr_light/scientific_number_spec.rb
@@ -40,6 +40,7 @@ describe DrLight::ScientificNumber do
           expect(number.to_s).to eq('-1.2')
         end
       end
+
       context 'when negative number only has 2 digits and order 10^-1' do
         let(:value) { -0.12 }
 
@@ -87,6 +88,7 @@ describe DrLight::ScientificNumber do
           expect(number.to_s).to eq('1.2')
         end
       end
+
       context 'when positive number only has 2 digits and order 10^-1' do
         let(:value) { 0.12 }
 
@@ -100,6 +102,116 @@ describe DrLight::ScientificNumber do
 
         it 'shows 2 digits' do
           expect(number.to_s).to eq('1.2e-1')
+        end
+      end
+    end
+
+    context 'when there deviance has the same order as the value' do
+      context 'when negative number only has 2 digits and order 10' do
+        let(:value)    { -12 }
+        let(:deviance) { 13 }
+
+        it 'shows 2 digits' do
+          expect(number.to_s).to eq('-1.2(13)e1')
+        end
+      end
+
+      context 'when negative number has 3 digits and order 10^2' do
+        let(:value)    { -123 }
+        let(:deviance) { 134 }
+
+        it 'shows 2 digits' do
+          expect(number.to_s).to eq('-1.2(13)e2')
+        end
+      end
+
+      context 'when negative number only has 2 digits and order 1' do
+        let(:value)    { -1.2 }
+        let(:deviance) { 1.3 }
+
+        it 'shows 2 digits' do
+          expect(number.to_s).to eq('-1.2(13)')
+        end
+      end
+
+      context 'when negative number has 3 digits and order 1' do
+        let(:value)    { -1.23 }
+        let(:deviance) { 1.34 }
+
+        it 'shows 2 digits' do
+          expect(number.to_s).to eq('-1.2(13)')
+        end
+      end
+
+      context 'when negative number only has 2 digits and order 10^-1' do
+        let(:value)    { -0.12 }
+        let(:deviance) { 0.13 }
+
+        it 'shows 2 digits' do
+          expect(number.to_s).to eq('-1.2(13)e-1')
+        end
+      end
+
+      context 'when negative number has 3 digits and order 10^-1' do
+        let(:value)    { -0.123 }
+        let(:deviance) { 0.134 }
+
+        it 'shows 2 digits' do
+          expect(number.to_s).to eq('-1.2(13)e-1')
+        end
+      end
+
+      context 'when positive number only has 2 digits and order 10' do
+        let(:value)    { 12 }
+        let(:deviance) { 13 }
+
+        it 'shows 2 digits' do
+          expect(number.to_s).to eq('1.2(13)e1')
+        end
+      end
+
+      context 'when positive number has 3 digits and order 10^2' do
+        let(:value)    { 123 }
+        let(:deviance) { 134 }
+
+        it 'shows 2 digits' do
+          expect(number.to_s).to eq('1.2(13)e2')
+        end
+      end
+
+      context 'when positive number only has 2 digits and order 1' do
+        let(:value)    { 1.2 }
+        let(:deviance) { 1.3 }
+
+        it 'shows 2 digits' do
+          expect(number.to_s).to eq('1.2(13)')
+        end
+      end
+
+      context 'when positive number has 3 digits and order 1' do
+        let(:value)    { 1.23 }
+        let(:deviance) { 1.34 }
+
+        it 'shows 2 digits' do
+          expect(number.to_s).to eq('1.2(13)')
+        end
+      end
+
+      context 'when positive number only has 2 digits and order 10^-1' do
+        let(:value)    { 0.12 }
+        let(:deviance) { 0.13 }
+
+        it 'shows 2 digits' do
+          expect(number.to_s).to eq('1.2(13)e-1')
+        end
+      end
+
+      context 'when positive number has 3 digits and order 10^-1' do
+        let(:value) { 0.123 }
+        let(:deviance) { 0.134 }
+
+        it 'shows 2 digits' do
+          expect(number.to_s).to eq('1.2(13)e-1')
         end
       end
     end
