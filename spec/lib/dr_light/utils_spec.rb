c38b8ab230be6c4ad620c7956173ba245aa75c32
darthjee <darthjee@gmail.com>
2019-12-26 23:45:55 +0100

    Add missing spec

--- spec/lib/dr_light/utils_spec.rb
+++ spec/lib/dr_light/utils_spec.rb
@@ -32,4 +32,64 @@ describe DrLight::Utils do
       end
     end
   end
+
+  describe '.order_difference' do
+    subject(:difference) do
+      described_class.order_difference(first, second)
+    end
+
+    context 'when first is bigger and has greater order' do
+      let(:first)  { 1000 }
+      let(:second) { 0.01 }
+
+      it 'returns positive' do
+        expect(difference).to be_positive
+      end
+    end
+
+    context 'when first is bigger but has lesser order' do
+      let(:first)  { 100 }
+      let(:second) { -1000 }
+
+      it 'returns positive' do
+        expect(difference).to be_negative
+      end
+    end
+
+    context 'when first is bigger but has same order' do
+      let(:first)  { 100 }
+      let(:second) { -100 }
+
+      it 'returns positive' do
+        expect(difference).to be_zero
+      end
+    end
+
+    context 'when first is smaller but has bigger order' do
+      let(:first)  { -100 }
+      let(:second) { 10 }
+
+      it 'returns positive' do
+        expect(difference).to be_positive
+      end
+    end
+
+    context 'when first is smaller and has smaller order' do
+      let(:first)  { -0.1 }
+      let(:second) { 10 }
+
+      it 'returns positive' do
+        expect(difference).to be_negative
+      end
+    end
+
+    context 'when first is smaller and has same order' do
+      let(:first)  { -0.1 }
+      let(:second) { 0.1 }
+
+      it 'returns positive' do
+        expect(difference).to be_zero
+      end
+    end
+  end
 end
