09008828b1149992ee9f88b3739550ce32fe7569
darthjee <darthjee@gmail.com>
2019-12-29 22:47:30 +0100

    Add deviance distance documentation

--- lib/dr_light/scientific_number/deviance_distance.rb
+++ lib/dr_light/scientific_number/deviance_distance.rb
@@ -2,7 +2,38 @@
 
 module DrLight
   class ScientificNumber
+    # @api private
+    #
+    # @author Dathjee
+    #
+    # Class Responsible for calculating distance between 2 numbers
+    # in number of deviances
+    #
+    # The combined deviance of numbers n1(d1), n2(d2) is
+    # d = sqrt(d1 ** 2 + d2 ** 2)
+    #
+    # @example With scientific number
+    #   number = DrLight::ScientificNumber.new(100, 3)
+    #   other  = DrLight::ScientificNumber.new(110, 4)
+    #
+    #   distance = DrLight::ScientificNumber::DevianceDistance.new(
+    #     number, other
+    #   )
+    #
+    #   distance.to_f    # returns 2
+    #
+    # @example With scientific number
+    #   number = DrLight::ScientificNumber.new(100, 3)
+    #   other  = 91
+    #
+    #   distance = DrLight::ScientificNumber::DevianceDistance.new(
+    #     number, other
+    #   )
+    #
+    #   distance.to_f    # returns 3
     class DevianceDistance
+      # @param number [ScientificNumber] first number
+      # @param second [ScientificNumber,Number] second number
       def initialize(number, other)
         @number = number
 
@@ -13,6 +44,9 @@ module DrLight
                  end
       end
 
+      # Calculates the distance of numbers in deviances
+      #
+      # @return [Number] always positive number
       def to_f
         return 0 if difference.zero?
 
